d14483b4ac428cc27d3c2df5ed2a7e3e548d5180
6-0-priyanka-shetty <samanthashettyshetty02@gmail.com>
2024-12-27 04:41:02 -0300

    Create check_weirdness.py

---
+++ check_weirdness.py
@@ -0,0 +1,12 @@
+def check_weirdness(n):
+  if n%2!=0:
+    print("weird")
+  elif 2 <= n <= 5 :
+    print("not weird ")
+  elif 6 <= n <= 20 :
+    print("weird")
+  else :
+    print("not weird")
+
+n =int(input("enter the number:")
+check_weirdness(n)
