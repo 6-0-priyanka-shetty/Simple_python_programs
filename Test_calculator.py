b1df4e4caa2a3f8ea9ec22aafebe3ec5ac542f5e
6-0-priyanka-shetty <samanthashettyshetty02@gmail.com>
2025-12-24 12:15:29 +0530

    Add files via upload
    here is math utils is the file that file i have imported in  Test_calculator.py  to "Testing"

---
+++ Test_calculator.py
@@ -0,0 +1,21 @@
+import unittest
+from math_utils import add,divide
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2,3),5)
+
+    def test_divide(self):
+        self.assertEqual(divide(10,2),5)
+
+    def test_valueError(self):
+        with self.assertRaises(ValueError):
+            a=int('four')
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide(10,0)
+
+
+if __name__=="__main__":
+    unittest.main()
---
+++ package.py
@@ -0,0 +1,4 @@
+from libraries import math_utils,Returns
+
+print(math_utils.add(2,3))
+print(Returns.res)
